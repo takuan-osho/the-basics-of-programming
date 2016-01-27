ac59b29400ef466261e9265cb2bda683c4c8adea
SHIMIZU Taku <shimizu.taku@gmail.com>
2016-01-27 15:49:14 +0900

    問題9.5を解いた

--- chap09/09-05.ml
+++ chap09/09-05.ml
@@ -14,3 +14,18 @@ let test1 = sum [] = 0
 let test2 = sum [2] = 2
 let test3 = sum [1; 3] = 4
 let test4 = sum [1; 2; 3; 4; 5; 6; 7; 8; 9; 10] = 55
+
+(* 目的: 整数のリストを受け取り、偶数の要素のみを含むリストを返す *)
+(* even : lst -> int list *)
+let rec even lst = match lst with
+| [] -> []
+| first :: rest -> if first mod 2 = 0 then first :: even rest
+                                      else even rest
+
+(* テスト *)
+let test1 = even [] = []
+let test2 = even [1] = []
+let test3 = even [2] = [2]
+let test4 = even [1; 2] = [2]
+let test5 = even [1; 2; 3] = [2]
+let test6 = even [1; 2; 3; 4] = [2; 4]
