b0df079fa2a981af87c1c881ae154873be3055f5
SHIMIZU Taku <shimizu.taku@gmail.com>
2016-02-21 20:45:30 +0900

    問題10.01を解いた

--- chap10/10-01.ml
+++ chap10/10-01.ml
@@ -10,6 +10,13 @@ let rec prefix lst = match lst with
 | [] -> []
 | first :: rest -> [first] :: add_to_each first (prefix rest)
 
+(* 目的: 受け取った昇順の整数リスト lst を受け取り、昇順となる位置に n を挿入する *)
+(* insert int list -> int -> int list *)
+let rec insert lst n = match lst with
+| [] -> [n]
+| first :: rest -> if first <= n then first :: (insert rest n)
+                                 else n :: first :: rest
+
 (* テスト *)
 let test1 = add_to_each 1 [] = []
 let test2 = add_to_each 1 [[2]] = [[1; 2]]
@@ -20,3 +27,8 @@ let test5 = prefix [] = []
 let test6 = prefix [1] = [[1]]
 let test7 = prefix [1; 2] = [[1]; [1; 2]]
 let test8 = prefix [1; 2; 3; 4] = [[1]; [1; 2]; [1; 2; 3]; [1; 2; 3; 4]]
+
+let test9 = insert [1; 3; 4] 2 = [1; 2; 3; 4]
+let test10 = insert [1; 3; 4; 7; 8] 5 = [1; 3; 4; 5; 7; 8]
+let test11 = insert [] 1 = [1]
+let test12 = insert [1; 2] 3 = [1; 2; 3]
