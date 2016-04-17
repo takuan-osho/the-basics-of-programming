5e5c4963624563226434a42f17cb001da5f33df6
SHIMIZU Taku <shimizu.taku@gmail.com>
2016-04-17 19:05:23 +0900

    問題10.4を解いた

--- chap10/10-04.ml
+++ chap10/10-04.ml
@@ -13,21 +13,21 @@ type person_t = {
 }
 
 let person1 = {
-  name = "山田太郎"; height = 1.73; weight = 66.0;
-  birthday = {month = 3; day = 20};
-  blood_type = "B"
-}
-
-let person2 = {
-  name = "鈴木宗近"; height = 1.88; weight = 75.2;
+  name = "a"; height = 1.88; weight = 75.2;
   birthday = {month = 6; day = 9};
   blood_type = "A"
 }
 
-let person3 = {
-  name = "田中春臣"; height = 1.66; weight = 67.0;
+let person2 = {
+  name = "c"; height = 1.66; weight = 67.0;
   birthday = {month = 1; day = 1};
   blood_type = "O"
+}
+
+let person3 = {
+  name = "f"; height = 1.73; weight = 66.0;
+  birthday = {month = 3; day = 20};
+  blood_type = "B"
 }
 
 (* person_t list 型のデータの例 *)
@@ -40,9 +40,19 @@ let lst6 = [person1; person2; person3]
 let lst7 = [person2; person1; person3]
 let lst8 = [person3; person2; person1]
 
+(* person_insert : person_t list -> person_t -> person_t list *)
+let rec person_insert lst person0 = match lst with
+| [] -> [person0]
+| ({name = name; birthday = birthday; blood_type = blood_type} as person) :: rest -> match person0 with
+  | {name = name0; birthday = birthday0; blood_type = blood_type0} -> if name < name0 then person :: person_insert rest person0
+                                  else person0 :: lst
+
+
 (* 目的: person_t 型のリストを受け取り、名前の順番にソートする *)
 (* person_sort : person_t list -> person_t list *)
-let person_sort lst = lst2
+let rec person_sort lst = match lst with
+| [] -> []
+| first :: rest -> person_insert (person_sort rest) first
 
 (* テスト *)
 let test1 = person_sort lst1 = []
