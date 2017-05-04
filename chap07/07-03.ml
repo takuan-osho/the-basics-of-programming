a3327a70947b28078f3d5a8052c60795ab3b7771
SHIMIZU Taku <shimizu.taku@gmail.com>
2017-05-04 09:17:30 +0900

    Fix typo

--- chap07/07-03.ml
+++ chap07/07-03.ml
@@ -1,4 +1,4 @@
-(* 目的: x, y座標の組となっている平面座標を受け取ったら、x軸について対象名点の座標を返す *)
+(* 目的: x, y座標の組となっている平面座標を受け取ったら、x軸について対象な点の座標を返す *)
 (* taisho_x : int * int -> int * int *)
 let taisho_x cooridinate = match cooridinate with
   (x, y) -> (-x, y)
