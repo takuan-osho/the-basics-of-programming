cfa729da6cdd97081fb7888a09dd574865941d93
SHIMIZU Taku <shimizu.taku@gmail.com>
2017-06-13 21:12:57 +0900

    例題13.1を写経

---
+++ chap13/example-13-01.ml
@@ -0,0 +1,14 @@
+(* 学生ひとり分のデータ(名前、点数、成績)を表す型 *)
+type gakusei_t = {
+  namae : string;
+  tensuu : int;
+  seiseki : string;
+}
+
+(* 目的: 学生リスト lst のうち成績が A の人の数を返す *)
+(* count_A : gakusei_t list -> int *)
+let rec count_A lst = match lst with
+  | [] -> 0
+  | {namae = n; tensuu = t; seiseki = s} :: rest
+    -> if s = "A" then 1 + count_A rest
+                  else count_A rest
